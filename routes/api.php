cc6db29e42e729cd08312a5104a6655523881c6c
bimasenaadji <bimasenaadji@gmail.com>
2025-05-28 18:47:59 +0700

    add controller & api user

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ChatController;
 use App\Http\Controllers\ItemController;
 use Illuminate\Support\Facades\Route;
+Use App\Http\Controllers\UserController;
 
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
@@ -19,3 +20,11 @@ Route::middleware(['auth:api'])->group(function () {
     Route::apiResource('item', ItemController::class);
     Route::apiResource('chat' , ChatController::class);
 });
+
+// User routes
+Route::get('user', [UserController::class, 'index']);
+Route::get('user/{id}', [UserController::class, 'show']);
+Route::post('user', [UserController::class, 'store']);
+Route::put('user/{id}', [UserController::class, 'update']);
+Route::delete('user/{id}', [UserController::class, 'destroy']);
+
