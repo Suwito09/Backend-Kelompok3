8a6970bbd706669a5c63cb138acaaec6a112fce2
Irsyad Fadhil <irsyadfadhil123@gmail.com>
2025-06-03 07:14:46 +0700

    update api routes

--- routes/api.php
+++ routes/api.php
@@ -1,30 +1,23 @@
 <?php
 
+use App\Http\Controllers\Admin\UserManagementController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ChatController;
 use App\Http\Controllers\ItemController;
+use App\Http\Controllers\ReturnController;
 use Illuminate\Support\Facades\Route;
-Use App\Http\Controllers\UserController;
 
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
-Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:api');
 
-Route::middleware(['auth:api', 'check.admin'])->group(function () {
-    Route::get('/admin-area', function () {
-        return response()->json(['message' => 'Welcome Admin!']);
-    });
+Route::prefix('admin')->middleware(['auth:api', 'check.admin'])->group(function () {
+    Route::apiResource('user', UserManagementController::class)->except('update');
 });
 
 Route::middleware(['auth:api'])->group(function () {
+    Route::post('logout', [AuthController::class, 'logout']);
     Route::apiResource('item', ItemController::class);
+    Route::apiResource('user', UserManagementController::class)->only('show', 'update');
+    Route::apiResource('return', ReturnController::class);
     Route::apiResource('chat' , ChatController::class);
 });
-
-// User routes
-Route::get('user', [UserController::class, 'index']);
-Route::get('user/{id}', [UserController::class, 'show']);
-Route::post('user', [UserController::class, 'store']);
-Route::put('user/{id}', [UserController::class, 'update']);
-Route::delete('user/{id}', [UserController::class, 'destroy']);
-
